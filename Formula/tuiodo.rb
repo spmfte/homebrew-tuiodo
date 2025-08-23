eeb9d08441f8155e8228b1e740981d94f0da1f00
Aidan Littman <35202874+spmfte@users.noreply.github.com>
2025-08-22 22:01:32 -0700

    Fix SHA256 for v1.1.3

--- Formula/tuiodo.rb
+++ Formula/tuiodo.rb
@@ -2,7 +2,7 @@ class Tuiodo < Formula
   desc "A modern terminal task manager with extensive customization"
   homepage "https://github.com/spmfte/tuiodo"
   url "https://github.com/spmfte/tuiodo/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "6186659032fa47685499ad52b59f9e4ea1c9c29dc18197b761d9497d46fe3581"
+  sha256 "e4015363f989c72cdd09388f5ed4ff81156016f04ae1d1d36e0ef283f637f4de"
   license "MIT"
   head "https://github.com/spmfte/tuiodo.git", branch: "master"
 
